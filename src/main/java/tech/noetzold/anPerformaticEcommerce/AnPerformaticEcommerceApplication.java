ffa2405095cd81abee91c9be995d15f2b395b450
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-28 09:30:11 -0300

    Initial commit

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/AnPerformaticEcommerceApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.anPerformaticEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnPerformaticEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnPerformaticEcommerceApplication.class, args);
+	}
+
+}
