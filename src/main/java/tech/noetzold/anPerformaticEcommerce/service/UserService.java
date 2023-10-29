684707123f23e1f9a2979f021a0f87ad1cb0a779
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-29 08:19:13 -0300

    Create UserService.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/UserService.java
@@ -0,0 +1,35 @@
+package tech.noetzold.anPerformaticEcommerce.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import tech.noetzold.anPerformaticEcommerce.model.user.ChangePasswordRequest;
+import tech.noetzold.anPerformaticEcommerce.model.user.User;
+import tech.noetzold.anPerformaticEcommerce.repository.UserRepository;
+
+import java.security.Principal;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository repository;
+    public void changePassword(ChangePasswordRequest request, Principal connectedUser) {
+
+        var user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+
+        if (!passwordEncoder.matches(request.getCurrentPassword(), user.getPassword())) {
+            throw new IllegalStateException("Wrong password");
+        }
+
+        if (!request.getNewPassword().equals(request.getConfirmationPassword())) {
+            throw new IllegalStateException("Password are not the same");
+        }
+
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+
+        repository.save(user);
+    }
+}
