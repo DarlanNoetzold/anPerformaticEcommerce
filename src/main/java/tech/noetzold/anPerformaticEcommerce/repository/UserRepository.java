b534a4570970422b01d9adff42053b129a66e816
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-29 08:18:50 -0300

    Update UserRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/UserRepository.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package tech.noetzold.anPerformaticEcommerce.repository;
 
-public class UserRepository {
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.noetzold.anPerformaticEcommerce.model.user.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
 }
