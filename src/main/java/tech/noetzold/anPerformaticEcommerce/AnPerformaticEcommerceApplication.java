f54984f8ede9060de10e93b6d5a47775e9ed5686
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-10-29 08:27:57 -0300

    Update AnPerformaticEcommerceApplication.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/AnPerformaticEcommerceApplication.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/AnPerformaticEcommerceApplication.java
@@ -1,13 +1,48 @@
 package tech.noetzold.anPerformaticEcommerce;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import tech.noetzold.anPerformaticEcommerce.model.user.RegisterRequest;
+import tech.noetzold.anPerformaticEcommerce.service.AuthenticationService;
+
+import static tech.noetzold.anPerformaticEcommerce.model.user.Role.ADMIN;
+import static tech.noetzold.anPerformaticEcommerce.model.user.Role.MANAGER;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class AnPerformaticEcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnPerformaticEcommerceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(
+			AuthenticationService service
+	) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("admin@mail.com")
+					.password("password")
+					.role(ADMIN)
+					.build();
+			System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+			var manager = RegisterRequest.builder()
+					.firstname("Admin")
+					.lastname("Admin")
+					.email("manager@mail.com")
+					.password("password")
+					.role(MANAGER)
+					.build();
+			System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+		};
 	}
 
 }
