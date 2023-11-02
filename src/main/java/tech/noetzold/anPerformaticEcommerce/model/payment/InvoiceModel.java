9b8dddf8cc0ff9be7a543c67c932484045872631
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-02 16:06:58 -0300

    Create InvoiceModel.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/payment/InvoiceModel.java
@@ -0,0 +1,56 @@
+package tech.noetzold.anPerformaticEcommerce.model.payment;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.wildfly.common.annotation.NotNull;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class InvoiceModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID invoiceId;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private CustomerModel customer;
+
+    @NotNull
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "paymentId")
+    private PaymentModel payment;
+
+    @NotNull
+    private String invoiceNumber;
+
+    @NotNull
+    private Date invoiceDate;
+
+    @NotNull
+    private double totalAmount;
+
+    private double discountAmount;
+
+    private String sellerName;
+
+    private String sellerAddress;
+
+    private String buyerName;
+
+    private String buyerAddress;
+
+    private List<String> itemsId;
+
+}
