86de5234b781b0ea3f12eb1dd9735a9e25ed1a6f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-02 16:07:01 -0300

    Create BoletoModel.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/payment/paymentMethods/BoletoModel.java
@@ -0,0 +1,44 @@
+package tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class BoletoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID boletoId;
+    private String codigoDeBarras;
+    private String linhaDigitavel;
+    private String beneficiarioNome;
+    private String beneficiarioCnpjCpf;
+    private String beneficiarioEndereco;
+    private String sacadoNome;
+    private String sacadoCnpjCpf;
+    private String sacadoEndereco;
+    private double valor;
+    private Date dataVencimento;
+    private String numeroDocumento;
+    private String instrucoesPagamento;
+    private double multa;
+    private double jurosAtraso;
+    private String logoBanco;
+    private String codigoBanco;
+    private String identificacaoBoleto;
+    private Date dataEmissao;
+    private String localPagamento;
+    private String codigoAutenticacao;
+    private String informacoesContato;
+}
