60503f5d847caf2461cf3a82313b1c02a2d08a02
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-02 16:07:23 -0300

    Create AddressModel.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/shipping/AddressModel.java
@@ -0,0 +1,63 @@
+package tech.noetzold.anPerformaticEcommerce.model.shipping;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.wildfly.common.annotation.NotNull;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AddressModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID addressId;
+
+    @NotNull
+    private String postaCode;
+
+    @NotNull
+    private String address1;
+
+    private String address2;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String uf;
+
+    @NotNull
+    private String country;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private double finalShippingCost;
+
+    @NotNull
+    private String phoneNumber;
+
+    @NotNull
+    private Date deliveryDate;
+
+    @NotNull
+    private boolean riskArea;
+
+    @NotNull
+    private String userId;
+
+}
