9e14ce2c097d31cc3c3a19d75c5f81f269e990e6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-02 16:06:42 -0300

    Create ProductModel.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/catalog/ProductModel.java
@@ -0,0 +1,70 @@
+package tech.noetzold.anPerformaticEcommerce.model.catalog;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.wildfly.common.annotation.NotNull;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ProductModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID productId;
+
+    private String title;
+
+    private String description;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private CategoryModel category;
+
+    private String brand;
+
+    private String model;
+
+    private String videoUrl;
+
+    private String gender;
+
+    private Integer warrantyTime;
+
+    private String warrantyText;
+
+    private Number height;
+
+    private Number width;
+
+    private Number weight;
+
+    private Number length;
+
+    private Number priceFactor;
+
+    private Boolean calculatedPrice;
+
+    private String definitionPriceScope;
+
+    private Boolean hasVariations;
+
+    private Boolean allowAutomaticSkuMarketplaceCreation;
+
+    private String code;
+
+    private String message;
+
+    private Boolean integrated;
+
+    private Date integratedDate;
+
+}
