642df109c194bab6c2cd2e16990a6cc00ba4585c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-02 16:07:06 -0300

    Create PaypalModel.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/payment/paymentMethods/PaypalModel.java
@@ -0,0 +1,25 @@
+package tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class PaypalModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID paypalId;
+    private String email;
+    private String descricao;
+    private String identificadorTransacao;
+}
