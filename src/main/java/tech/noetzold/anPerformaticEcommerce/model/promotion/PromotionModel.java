44080ce99d94cc62b29e016682db9ebebd575a2c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-02 16:07:19 -0300

    Create PromotionModel.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/promotion/PromotionModel.java
@@ -0,0 +1,40 @@
+package tech.noetzold.anPerformaticEcommerce.model.promotion;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import tech.noetzold.anPerformaticEcommerce.model.promotion.enums.PromotionType;
+import tech.noetzold.anPerformaticEcommerce.model.promotion.enums.ValueType;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class PromotionModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID promoId;
+    private String promoName;
+    private String promoDescription;
+    private ValueType valueType;
+    private Double value;
+    private PromotionType promotionType;
+    private String promoCompDesc;
+    private Double promoCompType;
+    private Double promoDtlId;
+    private String rules;
+    private Date startDate;
+    private Date endDate;
+    private String applyToCode;
+    private Double discountLimit;
+    private String exceptionParentId;
+
+}
