b06896ceab267f3138e4db0220f17c88df5f2816
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-02 16:07:13 -0300

    Create CouponModel.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/promotion/CouponModel.java
@@ -0,0 +1,28 @@
+package tech.noetzold.anPerformaticEcommerce.model.promotion;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.wildfly.common.annotation.NotNull;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CouponModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID couponId;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "promoId")
+    private PromotionModel promotion;
+
+    private String code;
+
+}
