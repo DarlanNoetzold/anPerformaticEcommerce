f8bf872f158328268b5f0f462f7f3fe5fefef77f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-03 08:24:39 -0300

    Update Order.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/model/Order.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/Order.java
@@ -1,4 +1,22 @@
 package tech.noetzold.anPerformaticEcommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID orderId;
 }
