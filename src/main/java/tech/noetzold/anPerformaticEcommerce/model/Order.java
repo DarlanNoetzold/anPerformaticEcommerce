61024cceeb986999bb1fea8317602ce4d1e2934a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-03 09:15:21 -0300

    Update Order.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/model/Order.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/Order.java
@@ -1,13 +1,14 @@
 package tech.noetzold.anPerformaticEcommerce.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import tech.noetzold.anPerformaticEcommerce.model.payment.PaymentModel;
+import tech.noetzold.anPerformaticEcommerce.model.shipping.ShippingModel;
 
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -19,4 +20,26 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID orderId;
+
+    @ManyToOne
+    private CustomerModel customerModel;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<PaymentModel> paymentModels;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<ShippingModel> shippingModels;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<CommerceItem> commerceItems;
+
+    @Temporal(TemporalType.DATE)
+    private Date initDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date lastUpdate;
+
+    private OrderState orderState;
+
+
 }
