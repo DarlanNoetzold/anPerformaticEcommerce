985c79193356cc8380fbd635bb970b4c7ca0d645
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 17:39:55 -0300

    Delete ProductModelRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/ProductModelRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.anPerformaticEcommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.anPerformaticEcommerce.model.catalog.ProductModel;
-
-import java.util.UUID;
-
-public interface ProductModelRepository extends JpaRepository<ProductModel, UUID> {
-}
