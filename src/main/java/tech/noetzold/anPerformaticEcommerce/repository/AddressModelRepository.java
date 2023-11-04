3b35bd9320737a8f4e1ef22cab58468c20817ff6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 17:39:08 -0300

    Delete AddressModelRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/AddressModelRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.anPerformaticEcommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.anPerformaticEcommerce.model.shipping.AddressModel;
-
-import java.util.UUID;
-
-public interface AddressModelRepository extends JpaRepository<AddressModel, UUID> {
-}
