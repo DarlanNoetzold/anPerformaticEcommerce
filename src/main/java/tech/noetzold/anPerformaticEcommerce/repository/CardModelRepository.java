ea1346af4491ba99c5db0629ceaa97fb65824113
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 17:39:13 -0300

    Delete CardModelRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/CardModelRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.anPerformaticEcommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods.CardModel;
-
-import java.util.UUID;
-
-public interface CardModelRepository extends JpaRepository<CardModel, UUID> {
-}
