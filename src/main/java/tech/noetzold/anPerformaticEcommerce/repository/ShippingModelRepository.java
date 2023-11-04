4909f92488cf9f40258ca7b16823ad484d89e425
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 10:46:23 -0300

    Create ShippingModelRepository.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/repository/ShippingModelRepository.java
@@ -0,0 +1,9 @@
+package tech.noetzold.anPerformaticEcommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.noetzold.anPerformaticEcommerce.model.shipping.ShippingModel;
+
+import java.util.UUID;
+
+public interface ShippingModelRepository extends JpaRepository<ShippingModel, UUID> {
+}
