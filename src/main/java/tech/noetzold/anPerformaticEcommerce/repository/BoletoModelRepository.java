2bd92972daf75a538abb0da1bb50ee030d364da8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 17:39:10 -0300

    Delete BoletoModelRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/BoletoModelRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.anPerformaticEcommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods.BoletoModel;
-
-import java.util.UUID;
-
-public interface BoletoModelRepository extends JpaRepository<BoletoModel, UUID> {
-}
