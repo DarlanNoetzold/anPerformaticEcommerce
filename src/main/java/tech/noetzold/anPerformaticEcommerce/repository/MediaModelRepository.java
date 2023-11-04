4986c9c85c99800f928d223f7d411474217ab9dc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 17:39:34 -0300

    Delete MediaModelRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/MediaModelRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.anPerformaticEcommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.anPerformaticEcommerce.model.catalog.MediaModel;
-
-import java.util.UUID;
-
-public interface MediaModelRepository extends JpaRepository<MediaModel, UUID> {
-}
