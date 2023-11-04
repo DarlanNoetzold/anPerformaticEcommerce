052ce44b11d3506f87859d8d4d96cebe89dbcca0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 10:45:49 -0300

    Create AttributeModelRepository.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/repository/AttributeModelRepository.java
@@ -0,0 +1,9 @@
+package tech.noetzold.anPerformaticEcommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.noetzold.anPerformaticEcommerce.model.catalog.AttributeModel;
+
+import java.util.UUID;
+
+public interface AttributeModelRepository extends JpaRepository<AttributeModel, UUID> {
+}
