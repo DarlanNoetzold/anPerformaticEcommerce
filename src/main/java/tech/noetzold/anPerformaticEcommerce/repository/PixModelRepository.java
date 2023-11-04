015c78a4e5f618c54edb0f1ba1932bf660b04312
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 17:39:53 -0300

    Delete PixModelRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/PixModelRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.anPerformaticEcommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods.PixModel;
-
-import java.util.UUID;
-
-public interface PixModelRepository extends JpaRepository<PixModel, UUID> {
-}
