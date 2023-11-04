778f210d2b27a65c225a9c44c31da2e95249c19b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 17:39:26 -0300

    Delete CategoryModelRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/CategoryModelRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.anPerformaticEcommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.anPerformaticEcommerce.model.catalog.CategoryModel;
-
-import java.util.UUID;
-
-public interface CategoryModelRepository extends JpaRepository<CategoryModel, UUID> {
-}
