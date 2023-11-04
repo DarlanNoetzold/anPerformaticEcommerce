0151d71dc343d79da68d71ab9501ace67ebb1204
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 17:39:51 -0300

    Delete PaypalModelRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/PaypalModelRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.anPerformaticEcommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods.PaypalModel;
-
-import java.util.UUID;
-
-public interface PaypalModelRepository extends JpaRepository<PaypalModel, UUID> {
-}
