4fe18a2e159b945559d01d430e97460e71a87597
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 17:39:32 -0300

    Delete KeyWordModelRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/KeyWordModelRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.noetzold.anPerformaticEcommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.anPerformaticEcommerce.model.catalog.KeyWordModel;
-
-import java.util.UUID;
-
-public interface KeyWordModelRepository extends JpaRepository<KeyWordModel, UUID> {
-}
