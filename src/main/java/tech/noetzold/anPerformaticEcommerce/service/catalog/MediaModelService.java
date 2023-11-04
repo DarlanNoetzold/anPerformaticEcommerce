39b9128e05a124287a3bd530cd3ad4c16055fb6a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 18:05:04 -0300

    Update MediaModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/catalog/MediaModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/catalog/MediaModelService.java
@@ -1,7 +1,12 @@
 package tech.noetzold.anPerformaticEcommerce.service.catalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.noetzold.anPerformaticEcommerce.repository.catalog.MediaModelRepository;
 
 @Service
 public class MediaModelService {
+
+    @Autowired
+    MediaModelRepository mediaModelRepository;
 }
