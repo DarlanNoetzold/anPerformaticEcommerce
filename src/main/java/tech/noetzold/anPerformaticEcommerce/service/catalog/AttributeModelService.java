f9ac5120605a3dd8f0c04d9cad46b0db13f42b0c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 18:04:57 -0300

    Update AttributeModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/catalog/AttributeModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/catalog/AttributeModelService.java
@@ -1,7 +1,12 @@
 package tech.noetzold.anPerformaticEcommerce.service.catalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.noetzold.anPerformaticEcommerce.repository.catalog.AttributeModelRepository;
 
 @Service
 public class AttributeModelService {
+
+    @Autowired
+    AttributeModelRepository attributeModelRepository;
 }
