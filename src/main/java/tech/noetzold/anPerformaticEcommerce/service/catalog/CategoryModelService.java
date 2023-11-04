4563f72de931c2b24edbeec8de0d5e76721ad1c3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 18:05:00 -0300

    Update CategoryModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/catalog/CategoryModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/catalog/CategoryModelService.java
@@ -1,7 +1,14 @@
 package tech.noetzold.anPerformaticEcommerce.service.catalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tech.noetzold.anPerformaticEcommerce.repository.catalog.CategoryModelRepository;
 
 @Service
 public class CategoryModelService {
+
+    @Autowired
+    CategoryModelRepository categoryModelRepository;
+
+
 }
