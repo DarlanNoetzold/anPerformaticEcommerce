e5a169e8c36b864a31363433c69d3136ff547fb8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-04 10:46:17 -0300

    Create ProductModelRepository.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/repository/ProductModelRepository.java
@@ -0,0 +1,9 @@
+package tech.noetzold.anPerformaticEcommerce.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.noetzold.anPerformaticEcommerce.model.catalog.ProductModel;
+
+import java.util.UUID;
+
+public interface ProductModelRepository extends JpaRepository<ProductModel, UUID> {
+}
