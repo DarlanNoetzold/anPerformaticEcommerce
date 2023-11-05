5819f407c755418793a5febcd1bb2cd25cf73fd1
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-05 14:01:48 -0300

    Create PromotionModelController.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/promotion/PromotionModelController.java
@@ -0,0 +1,11 @@
+package tech.noetzold.anPerformaticEcommerce.controller.promotion;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/ecommerce/v1/promo/promotion")
+public class PromotionModelController {
+}
