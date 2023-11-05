98399097b98ded7f7a051068b40ed6a87df8fb4b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-05 14:01:19 -0300

    Create KeyWordModelController.java

---
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/KeyWordModelController.java
@@ -0,0 +1,11 @@
+package tech.noetzold.anPerformaticEcommerce.controller.catalog;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/ecommerce/v1/catalog/keyword")
+public class KeyWordModelController {
+}
