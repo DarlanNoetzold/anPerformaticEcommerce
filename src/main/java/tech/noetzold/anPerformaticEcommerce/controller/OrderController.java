0d78c114c0ad51e6a7042741d9a6f0adacb267b9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:07:57 -0300

    Update OrderController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/OrderController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/OrderController.java
@@ -1,11 +1,16 @@
 package tech.noetzold.anPerformaticEcommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.anPerformaticEcommerce.service.OrderService;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ecommerce/v1/order")
 public class OrderController {
+
+    @Autowired
+    OrderService orderService;
 }
