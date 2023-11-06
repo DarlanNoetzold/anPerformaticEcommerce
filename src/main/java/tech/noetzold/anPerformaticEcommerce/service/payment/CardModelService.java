2c3875e1633b01f8eab62c298401bd8eaa239787
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:45 -0300

    Update CardModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/payment/CardModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/payment/CardModelService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service.payment;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods.CardModel;
 import tech.noetzold.anPerformaticEcommerce.repository.payment.CardModelRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("card")
 public class CardModelService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class CardModelService {
         return cardModelRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public CardModel saveCardModel(CardModel cardModel){
         return cardModelRepository.save(cardModel);
     }
 
+    @Transactional
     public void deleteCardModel(UUID id){
         cardModelRepository.deleteById(id);
     }
