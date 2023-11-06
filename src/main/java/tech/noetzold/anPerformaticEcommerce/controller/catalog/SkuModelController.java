3096bb7ad53624899c89a8c665f66ce35f630340
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:07:50 -0300

    Update SkuModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/SkuModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/SkuModelController.java
@@ -1,11 +1,16 @@
 package tech.noetzold.anPerformaticEcommerce.controller.catalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.anPerformaticEcommerce.service.catalog.SkuModelService;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ecommerce/v1/catalog/sku")
 public class SkuModelController {
+
+    @Autowired
+    SkuModelService skuModelService;
 }
