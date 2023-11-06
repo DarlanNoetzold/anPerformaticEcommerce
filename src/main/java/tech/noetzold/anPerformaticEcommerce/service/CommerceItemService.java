f401505fd7760d075994f3d99191ce8bd7312521
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:37 -0300

    Update CommerceItemService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/CommerceItemService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/CommerceItemService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.CommerceItem;
 import tech.noetzold.anPerformaticEcommerce.repository.CommerceItemRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("commerceitem")
 public class CommerceItemService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class CommerceItemService {
         return commerceItemRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public CommerceItem saveCommerceItem(CommerceItem commerceItem){
         return commerceItemRepository.save(commerceItem);
     }
 
+    @Transactional
     public void deleteCommerceItem(UUID id){
         commerceItemRepository.deleteById(id);
     }
