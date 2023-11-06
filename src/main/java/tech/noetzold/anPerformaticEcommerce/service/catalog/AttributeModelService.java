fd579d7cf06904107f1f0fe24b736c3bf7f21b03
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:25 -0300

    Update AttributeModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/catalog/AttributeModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/catalog/AttributeModelService.java
@@ -1,7 +1,9 @@
 package tech.noetzold.anPerformaticEcommerce.service.catalog;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tech.noetzold.anPerformaticEcommerce.model.catalog.AttributeModel;
 import tech.noetzold.anPerformaticEcommerce.repository.catalog.AttributeModelRepository;
 
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("attribute")
 public class AttributeModelService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class AttributeModelService {
         return attributeModelRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public AttributeModel saveAttributeModel(AttributeModel attributeModel){
         return attributeModelRepository.save(attributeModel);
     }
 
+    @Transactional
     public void deleteAttributeModel(UUID id){
         attributeModelRepository.deleteById(id);
     }
