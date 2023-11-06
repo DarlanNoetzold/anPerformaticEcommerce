bc1ac5d9f57a5dc7375f5f58368cbf3f1428e90b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:07:40 -0300

    Update AttributeModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/AttributeModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/AttributeModelController.java
@@ -1,11 +1,16 @@
 package tech.noetzold.anPerformaticEcommerce.controller.catalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.anPerformaticEcommerce.service.catalog.AttributeModelService;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ecommerce/v1/catalog/attribute")
 public class AttributeModelController {
+
+    @Autowired
+    AttributeModelService attributeModelService;
 }
