d6f323d0e0153c663f83d243a29b8659e5f1f47d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:07:32 -0300

    Update RedisConfig.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/cache/RedisConfig.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/cache/RedisConfig.java
@@ -16,7 +16,27 @@ public class RedisConfig {
     RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
         return (builder) -> {
             Map<String, RedisCacheConfiguration> configurationMap = new HashMap<>();
-            configurationMap.put("teste", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("attribute", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("category", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("keyword", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("media", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("product", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("sku", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("boleto", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("card", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("invoice", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("payment", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("paypal", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("pix", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("coupon", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("promotion", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("address", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("shipping", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("commerceitem", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("customer", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("order", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+            configurationMap.put("shopcart", RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofSeconds(20)));
+
             builder.withInitialCacheConfigurations(configurationMap);
         };
     }
