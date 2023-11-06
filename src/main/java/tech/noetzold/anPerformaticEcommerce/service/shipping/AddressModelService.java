4d57d7866eff814892ebe4e6ab0b181a0fdfc76f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:09:05 -0300

    Update AddressModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/shipping/AddressModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/shipping/AddressModelService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service.shipping;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.shipping.AddressModel;
 import tech.noetzold.anPerformaticEcommerce.repository.shipping.AddressModelRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("address")
 public class AddressModelService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class AddressModelService {
         return addressModelRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public AddressModel saveAddressModel(AddressModel addressModel){
         return addressModelRepository.save(addressModel);
     }
 
+    @Transactional
     public void deleteAddressModel(UUID id){
         addressModelRepository.deleteById(id);
     }
