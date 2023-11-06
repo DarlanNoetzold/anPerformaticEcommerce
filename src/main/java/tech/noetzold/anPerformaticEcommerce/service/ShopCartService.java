cbad421c83111c473f45aa88157ed241949cae67
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:09:13 -0300

    Update ShopCartService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/ShopCartService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/ShopCartService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.ShopCart;
 import tech.noetzold.anPerformaticEcommerce.repository.ShopCartRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("shopcart")
 public class ShopCartService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class ShopCartService {
         return shopCartRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public ShopCart saveShopCart(ShopCart shopCart){
         return shopCartRepository.save(shopCart);
     }
 
+    @Transactional
     public void deleteShopCart(UUID id){
         shopCartRepository.deleteById(id);
     }
