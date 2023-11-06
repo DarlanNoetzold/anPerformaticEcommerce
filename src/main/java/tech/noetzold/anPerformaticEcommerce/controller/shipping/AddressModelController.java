1d6184b11c6a4adacdc1fbeb1a000dc7327b1f41
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:07 -0300

    Update AddressModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/shipping/AddressModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/shipping/AddressModelController.java
@@ -1,11 +1,16 @@
 package tech.noetzold.anPerformaticEcommerce.controller.shipping;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.anPerformaticEcommerce.service.shipping.AddressModelService;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ecommerce/v1/shipping/address")
 public class AddressModelController {
+
+    @Autowired
+    AddressModelService addressModelService;
 }
