41ba4596ab92f351a5c751479444881278ae546c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:06 -0300

    Update PromotionModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/promotion/PromotionModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/promotion/PromotionModelController.java
@@ -1,11 +1,16 @@
 package tech.noetzold.anPerformaticEcommerce.controller.promotion;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.anPerformaticEcommerce.service.promotion.PromotionModelService;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ecommerce/v1/promo/promotion")
 public class PromotionModelController {
+
+    @Autowired
+    PromotionModelService promotionModelService;
 }
