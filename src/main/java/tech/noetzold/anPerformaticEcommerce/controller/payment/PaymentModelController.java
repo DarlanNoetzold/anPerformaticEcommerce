43e438949b33977bc1b959052a58e818fbcc1a2e
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:01 -0300

    Update PaymentModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/payment/PaymentModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/payment/PaymentModelController.java
@@ -1,11 +1,16 @@
 package tech.noetzold.anPerformaticEcommerce.controller.payment;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.anPerformaticEcommerce.service.payment.PaymentModelService;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ecommerce/v1/payment/payment")
 public class PaymentModelController {
+
+    @Autowired
+    PaymentModelService paymentModelService;
 }
