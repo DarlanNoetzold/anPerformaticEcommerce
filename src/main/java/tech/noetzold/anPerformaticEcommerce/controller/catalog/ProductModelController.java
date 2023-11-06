0e4c4e5fb0801290b0f0a1707a383aa83815774d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:07:47 -0300

    Update ProductModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/ProductModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/ProductModelController.java
@@ -1,11 +1,16 @@
 package tech.noetzold.anPerformaticEcommerce.controller.catalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.anPerformaticEcommerce.service.catalog.ProductModelService;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ecommerce/v1/catalog/product")
 public class ProductModelController {
+
+    @Autowired
+    ProductModelService productModelService;
 }
