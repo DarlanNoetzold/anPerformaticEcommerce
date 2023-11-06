6e04896b09a18ca39ad644609ca1c66fbdb4bd40
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:41 -0300

    Update OrderService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/OrderService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/OrderService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.Order;
 import tech.noetzold.anPerformaticEcommerce.repository.OrderRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("order")
 public class OrderService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class OrderService {
         return orderRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public Order saveOrder(Order order){
         return orderRepository.save(order);
     }
 
+    @Transactional
     public void deleteOrder(UUID id){
         orderRepository.deleteById(id);
     }
