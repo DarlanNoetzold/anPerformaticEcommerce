0168b4ca5f3145941a653f4db7bd919f66601f21
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:09:03 -0300

    Update PromotionModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/promotion/PromotionModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/promotion/PromotionModelService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service.promotion;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.promotion.PromotionModel;
 import tech.noetzold.anPerformaticEcommerce.repository.promotion.PromotionModelRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("promotion")
 public class PromotionModelService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class PromotionModelService {
         return promotionModelRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public PromotionModel savePromotionModel(PromotionModel promotionModel){
         return promotionModelRepository.save(promotionModel);
     }
 
+    @Transactional
     public void deletePromotionModel(UUID id){
         promotionModelRepository.deleteById(id);
     }
