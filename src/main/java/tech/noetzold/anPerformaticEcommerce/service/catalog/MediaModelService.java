08983a047846c6506e5b1eee8b39f6020d4044a9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:31 -0300

    Update MediaModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/catalog/MediaModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/catalog/MediaModelService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service.catalog;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.catalog.MediaModel;
 import tech.noetzold.anPerformaticEcommerce.repository.catalog.MediaModelRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("media")
 public class MediaModelService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class MediaModelService {
         return mediaModelRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public MediaModel saveMediaModel(MediaModel mediaModel){
         return mediaModelRepository.save(mediaModel);
     }
 
+    @Transactional
     public void deleteMediaModel(UUID id){
         mediaModelRepository.deleteById(id);
     }
