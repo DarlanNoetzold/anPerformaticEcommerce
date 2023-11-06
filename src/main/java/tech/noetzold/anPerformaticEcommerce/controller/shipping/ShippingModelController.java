562329929b561eae2b37ebfcc97f65755901712f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:09 -0300

    Update ShippingModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/shipping/ShippingModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/shipping/ShippingModelController.java
@@ -1,11 +1,17 @@
 package tech.noetzold.anPerformaticEcommerce.controller.shipping;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.anPerformaticEcommerce.model.shipping.ShippingModel;
+import tech.noetzold.anPerformaticEcommerce.service.shipping.ShippingModelService;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/ecommerce/v1/shipping/shipping")
 public class ShippingModelController {
+
+    @Autowired
+    ShippingModelService shippingModelService;
 }
