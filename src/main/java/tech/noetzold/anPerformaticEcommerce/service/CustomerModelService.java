1540fb2ce80ced2207ec27e481c771939628c171
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:39 -0300

    Update CustomerModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/CustomerModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/CustomerModelService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.CustomerModel;
 import tech.noetzold.anPerformaticEcommerce.repository.CustomerModelRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("customer")
 public class CustomerModelService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class CustomerModelService {
         return customerModelRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public CustomerModel saveCustomerModel(CustomerModel customerModel){
         return customerModelRepository.save(customerModel);
     }
 
+    @Transactional
     public void deleteCustomerModel(UUID id){
         customerModelRepository.deleteById(id);
     }
