0353eebde2e9c11e7a8853719becd65ca35424cf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:09:00 -0300

    Update PixModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/payment/PixModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/payment/PixModelService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service.payment;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods.PixModel;
 import tech.noetzold.anPerformaticEcommerce.repository.payment.PixModelRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("pix")
 public class PixModelService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class PixModelService {
         return pixModelRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public PixModel savePixModel(PixModel pixModel){
         return pixModelRepository.save(pixModel);
     }
 
+    @Transactional
     public void deletePixModel(UUID id){
         pixModelRepository.deleteById(id);
     }
