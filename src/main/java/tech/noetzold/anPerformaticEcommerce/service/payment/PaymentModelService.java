26c8ba3752b03f54263f6b6684c28209d582c509
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:51 -0300

    Update PaymentModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/payment/PaymentModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/payment/PaymentModelService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service.payment;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.payment.PaymentModel;
 import tech.noetzold.anPerformaticEcommerce.repository.payment.PaymentModelRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("payment")
 public class PaymentModelService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class PaymentModelService {
         return paymentModelRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public PaymentModel savePaymentModel(PaymentModel paymentModel){
         return paymentModelRepository.save(paymentModel);
     }
 
+    @Transactional
     public void deletePaymentModel(UUID id){
         paymentModelRepository.deleteById(id);
     }
