dc632476fefae563b87335c5e984e2f06c736cbe
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-06 07:08:42 -0300

    Update BoletoModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/payment/BoletoModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/payment/BoletoModelService.java
@@ -1,6 +1,8 @@
 package tech.noetzold.anPerformaticEcommerce.service.payment;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods.BoletoModel;
 import tech.noetzold.anPerformaticEcommerce.repository.payment.BoletoModelRepository;
@@ -9,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Cacheable("boleto")
 public class BoletoModelService {
 
     @Autowired
@@ -22,10 +25,12 @@ public class BoletoModelService {
         return boletoModelRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public BoletoModel saveBoletoModel(BoletoModel boletoModel){
         return boletoModelRepository.save(boletoModel);
     }
 
+    @Transactional
     public void deleteBoletoModel(UUID id){
         boletoModelRepository.deleteById(id);
     }
