5edfb9b19c9d1ab20156cd63cd433879f3243b96
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-07 20:30:51 -0300

    Update CouponModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/promotion/CouponModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/promotion/CouponModelController.java
@@ -1,10 +1,16 @@
 package tech.noetzold.anPerformaticEcommerce.controller.promotion;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.anPerformaticEcommerce.model.promotion.CouponModel;
 import tech.noetzold.anPerformaticEcommerce.service.promotion.CouponModelService;
+
+import java.util.Collection;
+import java.util.UUID;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -13,4 +19,54 @@ public class CouponModelController {
 
     @Autowired
     CouponModelService couponModelService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CouponModelController.class);
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Collection<CouponModel>> getAll() {
+        Collection<CouponModel> couponModels = couponModelService.findAllCouponModel();
+        if (couponModels.isEmpty()) {
+            logger.warn("No couponModel register");
+            return new ResponseEntity<Collection<CouponModel>>(HttpStatus.NO_CONTENT);
+        }
+
+        logger.info("couponModels "+couponModels.size()+" returned");
+        return new ResponseEntity<Collection<CouponModel>>(couponModels, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<CouponModel> getCouponModelById(@PathVariable("id") String id) {
+        CouponModel couponModel = couponModelService.findCouponModelById(UUID.fromString(id));
+        if (couponModel == null) {
+            logger.warn("couponModel not found");
+            return new ResponseEntity<CouponModel>(HttpStatus.NOT_FOUND);
+        }
+        logger.info("couponModel "+couponModel.getCouponId()+" returned");
+        return new ResponseEntity<CouponModel>(couponModel, HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<CouponModel> save(@RequestBody CouponModel couponModel) {
+        if (couponModel == null) {
+            logger.warn("couponModel is null");
+            return new ResponseEntity<CouponModel>(HttpStatus.BAD_REQUEST);
+        }
+
+        couponModel = couponModelService.saveCouponModel(couponModel);
+        logger.info("Create couponModel: " + couponModel);
+        return new ResponseEntity<CouponModel>(couponModel, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> remover(@PathVariable("id") String id) {
+        try {
+            couponModelService.deleteCouponModel(UUID.fromString(id));
+        }catch (Exception ex){
+            logger.error("Error to remove couponModel", ex);
+            return new ResponseEntity<String>(id, HttpStatus.BAD_GATEWAY);
+        }
+
+        logger.info("Remove couponModel: " + id);
+        return new ResponseEntity<String>(id, HttpStatus.OK);
+    }
 }
