bbb3daacc6401d4a5b1a9f9b64a289f13d206908
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-10 07:41:28 -0300

    Update SkuModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/SkuModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/SkuModelController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.anPerformaticEcommerce.model.catalog.SkuModel;
 import tech.noetzold.anPerformaticEcommerce.service.catalog.SkuModelService;
@@ -13,6 +14,7 @@ import java.util.Collection;
 import java.util.UUID;
 
 @CrossOrigin(origins = "*")
+@PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
 @RestController
 @RequestMapping("/ecommerce/v1/catalog/sku")
 public class SkuModelController {
