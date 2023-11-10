a78a89f7903ed1e42b4a69ebfc658e56e346a957
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-10 07:52:04 -0300

    Update AttributeModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/AttributeModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/catalog/AttributeModelController.java
@@ -1,7 +1,5 @@
 package tech.noetzold.anPerformaticEcommerce.controller.catalog;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
