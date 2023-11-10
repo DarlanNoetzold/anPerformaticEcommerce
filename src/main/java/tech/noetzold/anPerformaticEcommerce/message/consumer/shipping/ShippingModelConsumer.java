401206f0a926d1d3992daa4cedba0d5aec35a965
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 21:32:41 -0300

    Update ShippingModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/shipping/ShippingModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/shipping/ShippingModelConsumer.java
@@ -1,4 +1,33 @@
 package tech.noetzold.anPerformaticEcommerce.message.consumer.shipping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
+import tech.noetzold.anPerformaticEcommerce.model.shipping.ShippingModel;
+import tech.noetzold.anPerformaticEcommerce.service.shipping.ShippingModelService;
+
 public class ShippingModelConsumer {
+
+    @Autowired
+    ShippingModelService shippingModelService;
+
+    private static final Logger logger = LoggerFactory.getLogger(ShippingModelConsumer.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.SHIPPING_QUEUE)
+    private void consumerShippingModel(String message) throws JsonProcessingException {
+        ShippingModel shippingModel = new ObjectMapper().readValue(message, ShippingModel.class);
+        try {
+            shippingModelService.saveShippingModel(shippingModel);
+            logger.info("Consume shippingModel - " + shippingModel.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume cerate message for shippingModel - " + shippingModel.toString(), ex);
+        }
+
+    }
 }
