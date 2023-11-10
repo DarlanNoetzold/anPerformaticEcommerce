8c79c92c0cb2b76715abdb7593f595162c095c46
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 21:32:39 -0300

    Update AddressModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/shipping/AddressModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/shipping/AddressModelConsumer.java
@@ -1,4 +1,33 @@
 package tech.noetzold.anPerformaticEcommerce.message.consumer.shipping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
+import tech.noetzold.anPerformaticEcommerce.model.shipping.AddressModel;
+import tech.noetzold.anPerformaticEcommerce.service.shipping.AddressModelService;
+
 public class AddressModelConsumer {
+
+    @Autowired
+    AddressModelService addressModelService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AddressModelConsumer.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.ADDRESS_QUEUE)
+    private void consumerAddressModel(String message) throws JsonProcessingException {
+        AddressModel addressModel = new ObjectMapper().readValue(message, AddressModel.class);
+        try {
+            addressModelService.saveAddressModel(addressModel);
+            logger.info("Consume addressModel - " + addressModel.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume cerate message for addressModel - " + addressModel.toString(), ex);
+        }
+
+    }
 }
