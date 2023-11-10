8eeee9758409a2bce208736a96946ec7e720460d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 21:45:12 -0300

    Update PaypalModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/payment/PaypalModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/payment/PaypalModelConsumer.java
@@ -1,4 +1,34 @@
 package tech.noetzold.anPerformaticEcommerce.message.consumer.payment;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
+import tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods.PaypalModel;
+import tech.noetzold.anPerformaticEcommerce.service.payment.PaypalModelService;
+
 public class PaypalModelConsumer {
+
+    @Autowired
+    PaypalModelService paypalModelService;
+
+    private static final Logger logger = LoggerFactory.getLogger(PaypalModelConsumer.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.PAYPAL_QUEUE)
+    private void consumerPaypalModel(String message) throws JsonProcessingException {
+        PaypalModel paypalModel = new ObjectMapper().readValue(message, PaypalModel.class);
+        try {
+            paypalModelService.savePaypalModel(paypalModel);
+            logger.info("Consume paypalModel - " + paypalModel.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume cerate message for paypalModel - " + paypalModel.toString(), ex);
+        }
+
+    }
+    
 }
