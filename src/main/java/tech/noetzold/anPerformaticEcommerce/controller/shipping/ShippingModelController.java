0b3b0ac73b03f1bb35090c0e71f9cb0378368863
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-10 07:41:46 -0300

    Update ShippingModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/shipping/ShippingModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/shipping/ShippingModelController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.anPerformaticEcommerce.model.shipping.ShippingModel;
 import tech.noetzold.anPerformaticEcommerce.model.shipping.ShippingModel;
@@ -14,6 +15,7 @@ import java.util.Collection;
 import java.util.UUID;
 
 @CrossOrigin(origins = "*")
+@PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
 @RestController
 @RequestMapping("/ecommerce/v1/shipping/shipping")
 public class ShippingModelController {
