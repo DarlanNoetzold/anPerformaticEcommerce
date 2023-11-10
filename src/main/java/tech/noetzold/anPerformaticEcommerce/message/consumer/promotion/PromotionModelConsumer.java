39cf7a16eaa9b7145a457b5626ef2286157da3b8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 21:45:20 -0300

    Update PromotionModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/promotion/PromotionModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/promotion/PromotionModelConsumer.java
@@ -1,4 +1,34 @@
 package tech.noetzold.anPerformaticEcommerce.message.consumer.promotion;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
+import tech.noetzold.anPerformaticEcommerce.model.promotion.PromotionModel;
+import tech.noetzold.anPerformaticEcommerce.service.promotion.PromotionModelService;
+
 public class PromotionModelConsumer {
+
+    @Autowired
+    PromotionModelService promotionModelService;
+
+    private static final Logger logger = LoggerFactory.getLogger(PromotionModelConsumer.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.PROMOTION_QUEUE)
+    private void consumerPromotionModel(String message) throws JsonProcessingException {
+        PromotionModel promotionModel = new ObjectMapper().readValue(message, PromotionModel.class);
+        try {
+            promotionModelService.savePromotionModel(promotionModel);
+            logger.info("Consume promotionModel - " + promotionModel.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume cerate message for promotionModel - " + promotionModel.toString(), ex);
+        }
+
+    }
+    
 }
