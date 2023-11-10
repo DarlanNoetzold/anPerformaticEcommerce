156eb8dd15957c34887a16f64b6179f3f941604f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 21:45:06 -0300

    Update CardModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/payment/CardModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/payment/CardModelConsumer.java
@@ -1,4 +1,34 @@
 package tech.noetzold.anPerformaticEcommerce.message.consumer.payment;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
+import tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods.CardModel;
+import tech.noetzold.anPerformaticEcommerce.service.payment.CardModelService;
+
 public class CardModelConsumer {
+
+    @Autowired
+    CardModelService cardModelService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CardModelConsumer.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.CARD_QUEUE)
+    private void consumerCardModel(String message) throws JsonProcessingException {
+        CardModel cardModel = new ObjectMapper().readValue(message, CardModel.class);
+        try {
+            cardModelService.saveCardModel(cardModel);
+            logger.info("Consume cardModel - " + cardModel.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume cerate message for cardModel - " + cardModel.toString(), ex);
+        }
+
+    }
+    
 }
