3d98c9ae6383ab9825e47e3fb63afc81486244fc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 21:45:08 -0300

    Update InvoiceModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/payment/InvoiceModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/payment/InvoiceModelConsumer.java
@@ -1,4 +1,34 @@
 package tech.noetzold.anPerformaticEcommerce.message.consumer.payment;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
+import tech.noetzold.anPerformaticEcommerce.model.payment.InvoiceModel;
+import tech.noetzold.anPerformaticEcommerce.service.payment.InvoiceModelService;
+
 public class InvoiceModelConsumer {
+
+    @Autowired
+    InvoiceModelService invoiceModelService;
+
+    private static final Logger logger = LoggerFactory.getLogger(InvoiceModelConsumer.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.INVOICE_QUEUE)
+    private void consumerInvoiceModel(String message) throws JsonProcessingException {
+        InvoiceModel invoiceModel = new ObjectMapper().readValue(message, InvoiceModel.class);
+        try {
+            invoiceModelService.saveInvoiceModel(invoiceModel);
+            logger.info("Consume invoiceModel - " + invoiceModel.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume cerate message for invoiceModel - " + invoiceModel.toString(), ex);
+        }
+
+    }
+    
 }
