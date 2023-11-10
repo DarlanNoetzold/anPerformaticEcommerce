4a9b6e0fd691a0f36a273637636512ce636bae8a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 21:32:35 -0300

    Update CommerceItemConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/CommerceItemConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/CommerceItemConsumer.java
@@ -1,4 +1,35 @@
 package tech.noetzold.anPerformaticEcommerce.message.consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.noetzold.anPerformaticEcommerce.controller.CommerceItemController;
+import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
+import tech.noetzold.anPerformaticEcommerce.model.CommerceItem;
+import tech.noetzold.anPerformaticEcommerce.service.CommerceItemService;
+
 public class CommerceItemConsumer {
+
+    @Autowired
+    CommerceItemService commerceItemService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CommerceItemConsumer.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.COMMERCE_ITEM_QUEUE)
+    private void consumerCommerceItem(String message) throws JsonProcessingException {
+        CommerceItem commerceItem = new ObjectMapper().readValue(message, CommerceItem.class);
+        try {
+            commerceItemService.saveCommerceItem(commerceItem);
+            logger.info("Consume commerceItem - " + commerceItem.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume cerate message for commerceItem - " + commerceItem.toString(), ex);
+        }
+
+    }
+    
 }
