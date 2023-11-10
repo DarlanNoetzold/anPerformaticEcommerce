d7453b3450a11050a4783acfb431bf10e357d095
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 21:45:15 -0300

    Update PixModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/payment/PixModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/payment/PixModelConsumer.java
@@ -1,4 +1,34 @@
 package tech.noetzold.anPerformaticEcommerce.message.consumer.payment;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
+import tech.noetzold.anPerformaticEcommerce.model.payment.paymentMethods.PixModel;
+import tech.noetzold.anPerformaticEcommerce.service.payment.PixModelService;
+
 public class PixModelConsumer {
+
+    @Autowired
+    PixModelService pixModelService;
+
+    private static final Logger logger = LoggerFactory.getLogger(PixModelConsumer.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.PIX_QUEUE)
+    private void consumerPixModel(String message) throws JsonProcessingException {
+        PixModel pixModel = new ObjectMapper().readValue(message, PixModel.class);
+        try {
+            pixModelService.savePixModel(pixModel);
+            logger.info("Consume pixModel - " + pixModel.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume cerate message for pixModel - " + pixModel.toString(), ex);
+        }
+
+    }
+    
 }
