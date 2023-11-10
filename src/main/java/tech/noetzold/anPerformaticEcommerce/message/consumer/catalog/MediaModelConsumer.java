807712d2e575f51733f73258d63cda1567b52956
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 22:09:31 -0300

    Update MediaModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/catalog/MediaModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/catalog/MediaModelConsumer.java
@@ -1,4 +1,36 @@
 package tech.noetzold.anPerformaticEcommerce.message.consumer.catalog;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
+import tech.noetzold.anPerformaticEcommerce.model.catalog.MediaModel;
+import tech.noetzold.anPerformaticEcommerce.service.catalog.MediaModelService;
+
+@Component
 public class MediaModelConsumer {
+
+    @Autowired
+    MediaModelService mediaModelService;
+
+    private static final Logger logger = LoggerFactory.getLogger(MediaModelConsumer.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.MEDIA_QUEUE)
+    private void consumerMediaModel(String message) throws JsonProcessingException {
+        MediaModel mediaModel = new ObjectMapper().readValue(message, MediaModel.class);
+        try {
+            mediaModelService.saveMediaModel(mediaModel);
+            logger.info("Consume mediaModel - " + mediaModel.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume cerate message for mediaModel - " + mediaModel.toString(), ex);
+        }
+
+    }
+    
 }
