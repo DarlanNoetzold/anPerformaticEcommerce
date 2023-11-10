1d38ecde71708da0670836588e9f4301b3f0cc15
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-10 07:41:42 -0300

    Update PromotionModelController.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/controller/promotion/PromotionModelController.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/controller/promotion/PromotionModelController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import tech.noetzold.anPerformaticEcommerce.model.promotion.PromotionModel;
 import tech.noetzold.anPerformaticEcommerce.service.promotion.PromotionModelService;
@@ -13,6 +14,7 @@ import java.util.Collection;
 import java.util.UUID;
 
 @CrossOrigin(origins = "*")
+@PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
 @RestController
 @RequestMapping("/ecommerce/v1/promo/promotion")
 public class PromotionModelController {
