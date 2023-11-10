5d064099c4cec3f2d32ff4ceb73da6dac4a4fe51
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-10 07:49:31 -0300

    Update AnPerformaticEcommerceApplication.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/AnPerformaticEcommerceApplication.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/AnPerformaticEcommerceApplication.java
@@ -1,8 +1,10 @@
 package tech.noetzold.anPerformaticEcommerce;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import tech.noetzold.anPerformaticEcommerce.security.user.RegisterRequest;
@@ -12,6 +14,8 @@ import static tech.noetzold.anPerformaticEcommerce.security.user.Role.ADMIN;
 import static tech.noetzold.anPerformaticEcommerce.security.user.Role.MANAGER;
 
 @SpringBootApplication
+@EnableRabbit
+@EnableCaching
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class AnPerformaticEcommerceApplication {
 
