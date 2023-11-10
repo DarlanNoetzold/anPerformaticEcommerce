0c4b9991e582a7024632d1c07f8180a02849d2ae
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 22:09:30 -0300

    Update KeyWordModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/catalog/KeyWordModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/catalog/KeyWordModelConsumer.java
@@ -1,4 +1,36 @@
 package tech.noetzold.anPerformaticEcommerce.message.consumer.catalog;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
+import tech.noetzold.anPerformaticEcommerce.model.catalog.KeyWordModel;
+import tech.noetzold.anPerformaticEcommerce.service.catalog.KeyWordModelService;
+
+@Component
 public class KeyWordModelConsumer {
+
+    @Autowired
+    KeyWordModelService keyWordModelService;
+
+    private static final Logger logger = LoggerFactory.getLogger(KeyWordModelConsumer.class);
+
+    @Transactional
+    @RabbitListener(queues = RabbitmqQueues.KEY_WORD_QUEUE)
+    private void consumerKeyWordModel(String message) throws JsonProcessingException {
+        KeyWordModel keyWordModel = new ObjectMapper().readValue(message, KeyWordModel.class);
+        try {
+            keyWordModelService.saveKeyWordModel(keyWordModel);
+            logger.info("Consume keyWordModel - " + keyWordModel.toString());
+        }catch (Exception ex){
+            logger.error("Error to consume cerate message for keyWordModel - " + keyWordModel.toString(), ex);
+        }
+
+    }
+    
 }
