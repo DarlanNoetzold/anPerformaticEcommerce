51242133885622b6f708144ac6c0d39cadcd4845
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-09 21:32:33 -0300

    Update RabbitmqQueues.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/config/RabbitmqQueues.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/config/RabbitmqQueues.java
@@ -4,4 +4,9 @@ public class RabbitmqQueues {
 
     public static final String SHOP_CART_QUEUE = "SHOP_CART";
     public static final String ORDER_QUEUE = "ORDER";
+    public static final String CUSTOMER_QUEUE = "CUSTOMER";
+    public static final String COMMERCE_ITEM_QUEUE = "COMMERCE_ITEM";
+    public static final String ADDRESS_QUEUE = "ADDRESS";
+    public static final String SHIPPING_QUEUE = "SHIPPING";
+
 }
