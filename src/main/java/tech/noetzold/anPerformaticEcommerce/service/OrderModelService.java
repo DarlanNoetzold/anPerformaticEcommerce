5d06d929d70e937ddb90a59bdc670c426b402202
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-13 19:23:20 -0300

    Update OrderModelService.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/service/OrderModelService.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/service/OrderModelService.java
@@ -4,37 +4,37 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
-import tech.noetzold.anPerformaticEcommerce.model.Order;
+import tech.noetzold.anPerformaticEcommerce.model.OrderModel;
 import tech.noetzold.anPerformaticEcommerce.repository.OrderRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
-@Cacheable("order")
+@Cacheable("order_model")
 public class OrderModelService {
 
     @Autowired
     OrderRepository orderRepository;
 
     @Transactional
-    public List<Order> findAllOrder(){
+    public List<OrderModel> findAllOrder(){
         return orderRepository.findAll();
     }
 
     @Transactional
-    public Order findOrderById(UUID id){
+    public OrderModel findOrderById(UUID id){
         return orderRepository.findById(id).orElse(null);
     }
 
     @Transactional
-    public Order updateOrder(UUID id, Order order){
+    public OrderModel updateOrder(UUID id, OrderModel order){
         order.setOrderId(id);
         return orderRepository.save(order);
     }
     
     @Transactional
-    public Order saveOrder(Order order){
+    public OrderModel saveOrder(OrderModel order){
         return orderRepository.save(order);
     }
 
