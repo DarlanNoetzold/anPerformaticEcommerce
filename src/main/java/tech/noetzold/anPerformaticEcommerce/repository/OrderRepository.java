bf589a60dc471134551f3a579c50790da041b541
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-13 19:23:22 -0300

    Update OrderRepository.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/repository/OrderRepository.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/repository/OrderRepository.java
@@ -1,9 +1,9 @@
 package tech.noetzold.anPerformaticEcommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.anPerformaticEcommerce.model.Order;
+import tech.noetzold.anPerformaticEcommerce.model.OrderModel;
 
 import java.util.UUID;
 
-public interface OrderRepository extends JpaRepository<Order, UUID> {
+public interface OrderRepository extends JpaRepository<OrderModel, UUID> {
 }
