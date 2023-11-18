7fbee78a0c4c68a24ec2790635d642f5fa254532
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-17 21:06:32 -0300

    Update ShopCart.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/model/ShopCart.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/ShopCart.java
@@ -5,13 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class ShopCart {
+public class ShopCart implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
