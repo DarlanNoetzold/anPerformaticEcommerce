4a6c434fd3b37745df25a70dd1a750438bea16c8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-17 21:07:04 -0300

    Update CommerceItem.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/model/CommerceItem.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/CommerceItem.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import tech.noetzold.anPerformaticEcommerce.model.catalog.SkuModel;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
@@ -14,7 +15,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class CommerceItem {
+public class CommerceItem implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
