4f8785955774bcbd235e04b81b8e93bcba125dcc
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-17 21:07:21 -0300

    Update CategoryModel.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/model/catalog/CategoryModel.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/model/catalog/CategoryModel.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.UUID;
 
@@ -16,7 +17,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class CategoryModel {
+public class CategoryModel implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
