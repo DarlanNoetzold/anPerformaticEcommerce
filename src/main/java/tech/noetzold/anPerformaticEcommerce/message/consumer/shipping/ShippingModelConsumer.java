0feb98baab5c81aa3e86501649a70d9b7575c462
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-20 14:51:07 -0300

    Update ShippingModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/shipping/ShippingModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/shipping/ShippingModelConsumer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -29,6 +30,7 @@ public class ShippingModelConsumer {
             logger.info("Consume shippingModel - " + shippingModel.toString());
         }catch (Exception ex){
             logger.error("Error to consume cerate message for shippingModel - " + shippingModel.toString(), ex);
+            throw new AmqpRejectAndDontRequeueException("Ops, an error! Message should go to DLQ");
         }
 
     }
