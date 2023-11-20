6b137c8e8151bad5e77970024b7106f70f22ef52
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-20 11:39:19 -0300

    Update CustomErrorStrategy.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/config/CustomErrorStrategy.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/config/CustomErrorStrategy.java
@@ -1,13 +1,52 @@
 package tech.noetzold.anPerformaticEcommerce.message.config;
 
-import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.listener.FatalExceptionStrategy;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.support.ListenerExecutionFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class CustomErrorStrategy extends ConditionalRejectingErrorHandler.DefaultExceptionStrategy {
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomErrorStrategy implements FatalExceptionStrategy {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    AmqpAdmin amqpAdmin;
+
+    private Queue deadLetterQueue(String queueName) {
+        Map<String, Object> args = new HashMap<>();
+        args.put("x-dead-letter-exchange", "amq.direct");
+        args.put("x-dead-letter-routing-key", queueName);
+        return QueueBuilder.durable(queueName + ".dlq")
+                .withArguments(args)
+                .build();
+    }
+
+    private void setupQueueWithDLQ(String queueName) {
+        Queue queue = new Queue(queueName, true, false, false);
+        Queue dlq = deadLetterQueue(queueName);
+        amqpAdmin.declareQueue(queue);
+        amqpAdmin.declareQueue(dlq);
+
+        DirectExchange exchange = new DirectExchange("amq.direct");
+        Binding binding = BindingBuilder.bind(queue).to(exchange).with(queue.getName());
+        amqpAdmin.declareExchange(exchange);
+        amqpAdmin.declareBinding(binding);
+    }
+
     @Override
     public boolean isFatal(Throwable t) {
-        System.out.println(new String(((ListenerExecutionFailedException) t).getFailedMessage().getBody()));
+        if (t instanceof ListenerExecutionFailedException) {
+            ListenerExecutionFailedException executionFailedException = (ListenerExecutionFailedException) t;
+            Message failedMessage = executionFailedException.getFailedMessage();
+            String queueName = failedMessage.getMessageProperties().getConsumerQueue();
 
-        return t.getCause() instanceof IllegalArgumentException;
+            rabbitTemplate.convertAndSend(queueName + ".dlq", failedMessage.getBody());
+        }
+        return true;
     }
 }
