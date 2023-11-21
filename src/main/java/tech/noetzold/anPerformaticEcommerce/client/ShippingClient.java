5873e562c1f02ff048ed5fbc8b234e0d6e8415a6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-21 18:08:56 -0300

    Update ShippingClient.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/client/ShippingClient.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/client/ShippingClient.java
@@ -1,4 +1,9 @@
 package tech.noetzold.anPerformaticEcommerce.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+
+@Service
+@FeignClient(url= "http://localhost:6000" , name = "shipping")
 public class ShippingClient {
 }
