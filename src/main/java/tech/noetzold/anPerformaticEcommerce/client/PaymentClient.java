7f93e864a5b241adcc3a9d302f49517043a7f8ef
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-21 18:08:50 -0300

    Update PaymentClient.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/client/PaymentClient.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/client/PaymentClient.java
@@ -1,4 +1,9 @@
 package tech.noetzold.anPerformaticEcommerce.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+
+@Service
+@FeignClient(url= "http://localhost:7000" , name = "payment")
 public class PaymentClient {
 }
