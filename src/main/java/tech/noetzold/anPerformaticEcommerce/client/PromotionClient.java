ae3848f7cc385fca3622df56ae22983fef49e019
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-21 18:08:54 -0300

    Update PromotionClient.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/client/PromotionClient.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/client/PromotionClient.java
@@ -1,4 +1,9 @@
 package tech.noetzold.anPerformaticEcommerce.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+
+@Service
+@FeignClient(url= "http://localhost:4000" , name = "promotion")
 public class PromotionClient {
 }
