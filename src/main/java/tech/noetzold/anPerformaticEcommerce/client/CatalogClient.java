e73f7db2988e9f4c29054b20d5b206450734d5b6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-21 18:08:49 -0300

    Update CatalogClient.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/client/CatalogClient.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/client/CatalogClient.java
@@ -1,4 +1,9 @@
 package tech.noetzold.anPerformaticEcommerce.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+
+@Service
+@FeignClient(url= "http://localhost:5000" , name = "catalog")
 public class CatalogClient {
 }
