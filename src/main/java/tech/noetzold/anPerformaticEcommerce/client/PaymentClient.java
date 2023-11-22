33e4c07e80789bc37cc4e64eb4e19bb317d38520
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-22 08:27:44 -0300

    Update PaymentClient.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/client/PaymentClient.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/client/PaymentClient.java
@@ -2,8 +2,30 @@ package tech.noetzold.anPerformaticEcommerce.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.anPerformaticEcommerce.model.payment.PaymentModel;
+
+import java.util.List;
 
 @Service
-@FeignClient(url= "http://localhost:7000" , name = "payment")
-public class PaymentClient {
+@FeignClient(url= "http://localhost:7000/api/payment/v1/payment" , name = "payment")
+public interface PaymentClient {
+
+    @GetMapping("/{id}")
+    PaymentModel getPayment(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestParam String id);
+
+    @GetMapping("/order/{orderId}")
+    List<PaymentModel> getAllByOrderIdPayment(@RequestHeader(value = "Authorization", required = true) String authorizationHeader,@RequestParam String orderId);
+
+    @GetMapping("/order/{userId}")
+    List<PaymentModel> getAllByUserIdPayment(@RequestHeader(value = "Authorization", required = true) String authorizationHeader,@RequestParam String userId);
+
+    @PostMapping()
+    PaymentModel savePayment(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestBody PaymentModel shippingModel);
+
+    @PutMapping("/{id}")
+    PaymentModel updatePayment(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestBody PaymentModel shippingModel,  @RequestParam String id);
+
+    @DeleteMapping("/{id}")
+    PaymentModel deletePayment(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestParam String id);
 }
