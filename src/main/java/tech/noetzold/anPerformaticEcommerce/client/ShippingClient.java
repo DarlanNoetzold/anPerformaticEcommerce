d52d2c2123ebd43c7a6202eab613ee5ae103ddf4
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-22 08:27:48 -0300

    Update ShippingClient.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/client/ShippingClient.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/client/ShippingClient.java
@@ -2,8 +2,27 @@ package tech.noetzold.anPerformaticEcommerce.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import tech.noetzold.anPerformaticEcommerce.model.shipping.ShippingModel;
+
+import java.util.List;
 
 @Service
-@FeignClient(url= "http://localhost:6000" , name = "shipping")
-public class ShippingClient {
+@FeignClient(url= "http://localhost:6000/api/shipping/v1/address" , name = "shipping")
+public interface ShippingClient {
+
+    @GetMapping("/{id}")
+    ShippingModel getShipping(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestParam String id);
+
+    @GetMapping("/order/{orderId}")
+    List<ShippingModel> getAllByOrderIdShipping(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestParam String orderId);
+
+    @PostMapping()
+    ShippingModel saveShipping(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestBody ShippingModel shippingModel);
+
+    @PutMapping("/{id}")
+    ShippingModel updateShipping(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestBody ShippingModel shippingModel,  @RequestParam String id);
+
+    @DeleteMapping("/{id}")
+    ShippingModel deleteShipping(@RequestHeader(value = "Authorization", required = true) String authorizationHeader, @RequestParam String id);
 }
