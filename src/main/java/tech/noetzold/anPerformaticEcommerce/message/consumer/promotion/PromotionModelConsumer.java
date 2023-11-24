a7a34678ddca17d83351d07e833adc80e047052a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-24 10:31:33 -0300

    Update PromotionModelConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/promotion/PromotionModelConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/promotion/PromotionModelConsumer.java
@@ -9,8 +9,10 @@ import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import tech.noetzold.anPerformaticEcommerce.client.PromotionClient;
 import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
 import tech.noetzold.anPerformaticEcommerce.model.promotion.PromotionModel;
+import tech.noetzold.anPerformaticEcommerce.service.LoginService;
 import tech.noetzold.anPerformaticEcommerce.service.promotion.PromotionModelService;
 
 @Component
@@ -18,6 +20,12 @@ public class PromotionModelConsumer {
 
     @Autowired
     PromotionModelService promotionModelService;
+
+    @Autowired
+    PromotionClient promotionClient;
+
+    @Autowired
+    LoginService loginService;
 
     private static final Logger logger = LoggerFactory.getLogger(PromotionModelConsumer.class);
 
@@ -27,6 +35,7 @@ public class PromotionModelConsumer {
         PromotionModel promotionModel = new ObjectMapper().readValue(message, PromotionModel.class);
         try {
             promotionModelService.savePromotionModel(promotionModel);
+            promotionClient.savePromotion(loginService.getToken(), promotionModel);
             logger.info("Consume promotionModel - " + promotionModel.toString());
         }catch (Exception ex){
             logger.error("Error to consume cerate message for promotionModel - " + promotionModel.toString(), ex);
