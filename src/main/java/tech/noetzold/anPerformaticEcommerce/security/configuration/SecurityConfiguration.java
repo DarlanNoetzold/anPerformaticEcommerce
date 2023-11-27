718605586f5b9bf8aedb1fa465e70aa6f5d2f24b
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-27 08:32:34 -0300

    Update SecurityConfiguration.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/security/configuration/SecurityConfiguration.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/security/configuration/SecurityConfiguration.java
@@ -35,7 +35,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-    private static final String[] WHITE_LIST_URL = {"/ecommerce/v1/auth/**",
+    private static final String[] WHITE_LIST_URL = {"/ecommerce/v1/auth/**", "/ecommerce/v1/auth/authenticate",
             "/v2/api-docs",
             "/v3/api-docs",
             "/v3/api-docs/**",
