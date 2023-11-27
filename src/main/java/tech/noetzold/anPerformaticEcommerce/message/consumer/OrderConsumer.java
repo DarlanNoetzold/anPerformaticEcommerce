a83073b8ec5adf9eebd8a4e8daeb96ef0af9f1e7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-27 07:03:12 -0300

    Update OrderConsumer.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/OrderConsumer.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/message/consumer/OrderConsumer.java
@@ -3,22 +3,40 @@ package tech.noetzold.anPerformaticEcommerce.message.consumer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import tech.noetzold.anPerformaticEcommerce.client.PaymentClient;
+import tech.noetzold.anPerformaticEcommerce.client.PromotionClient;
+import tech.noetzold.anPerformaticEcommerce.client.ShippingClient;
 import tech.noetzold.anPerformaticEcommerce.controller.OrderController;
 import tech.noetzold.anPerformaticEcommerce.message.config.RabbitmqQueues;
 import tech.noetzold.anPerformaticEcommerce.model.OrderModel;
+import tech.noetzold.anPerformaticEcommerce.model.payment.PaymentModel;
+import tech.noetzold.anPerformaticEcommerce.model.shipping.ShippingModel;
+import tech.noetzold.anPerformaticEcommerce.service.LoginService;
 import tech.noetzold.anPerformaticEcommerce.service.OrderModelService;
+
+import java.util.List;
 
 @Component
 public class OrderConsumer {
 
     @Autowired
     OrderModelService orderModelService;
+
+    @Autowired
+    ShippingClient shippingClient;
+
+    @Autowired
+    PaymentClient paymentClient;
+
+    @Autowired
+    LoginService loginService;
 
     private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
 
@@ -28,6 +46,7 @@ public class OrderConsumer {
         OrderModel order = new ObjectMapper().readValue(message, OrderModel.class);
         try {
             orderModelService.saveOrder(order);
+            sendShippingModel(order.getShippingModels());
             logger.info("Consume order - " + order.toString());
         }catch (Exception ex){
             logger.error("Error to consume cerate message for order - " + order.toString(), ex);
@@ -35,4 +54,17 @@ public class OrderConsumer {
         }
 
     }
+
+    public void sendShippingModel(List<ShippingModel> shippingModels) throws JSONException {
+        for (ShippingModel shippingModel: shippingModels) {
+            shippingClient.saveShipping(loginService.getToken(),shippingModel);
+        }
+    }
+
+    public void sendPaymentModel(List<PaymentModel> paymentModels) throws JSONException {
+        for (PaymentModel paymentModel: paymentModels) {
+            paymentClient.savePayment(loginService.getToken(), paymentModel);
+        }
+    }
+
 }
