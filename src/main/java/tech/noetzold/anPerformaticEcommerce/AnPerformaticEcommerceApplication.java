e4eeb25f999875dacedeb7b16a7081ca936fc30f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-27 07:21:53 -0300

    Update AnPerformaticEcommerceApplication.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/AnPerformaticEcommerceApplication.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/AnPerformaticEcommerceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import tech.noetzold.anPerformaticEcommerce.security.user.RegisterRequest;
 import tech.noetzold.anPerformaticEcommerce.security.service.AuthenticationService;
@@ -17,6 +18,8 @@ import static tech.noetzold.anPerformaticEcommerce.security.user.Role.MANAGER;
 @EnableRabbit
 @EnableCaching
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@ComponentScan({ "tech.noetzold.anPerformaticEcommerce.client.*", "tech.noetzold.anPerformaticEcommerce.security.service",
+		"tech.noetzold.anPerformaticEcommerce.security.configuration"  })
 public class AnPerformaticEcommerceApplication {
 
 	public static void main(String[] args) {
