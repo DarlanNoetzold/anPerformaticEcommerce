9ac2b5d2e2356cccdbd2607228f00e8fa636740a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-28 08:37:33 -0300

    Update AnPerformaticEcommerceApplication.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/AnPerformaticEcommerceApplication.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/AnPerformaticEcommerceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -17,9 +18,8 @@ import static tech.noetzold.anPerformaticEcommerce.security.user.Role.MANAGER;
 @SpringBootApplication
 @EnableRabbit
 @EnableCaching
+@EnableFeignClients(basePackages = {"tech.noetzold.anPerformaticEcommerce.client"})
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
-@ComponentScan({ "tech.noetzold.anPerformaticEcommerce.client", "tech.noetzold.anPerformaticEcommerce.security.service",
-		"tech.noetzold.anPerformaticEcommerce.security.configuration", "tech.noetzold.anPerformaticEcommerce.security.controller"  })
 public class AnPerformaticEcommerceApplication {
 
 	public static void main(String[] args) {
