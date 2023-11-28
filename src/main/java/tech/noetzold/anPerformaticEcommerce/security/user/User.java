86c9a425487ea52ae4796736ed1ff3599f6ed222
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-28 08:37:43 -0300

    Update User.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/security/user/User.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/security/user/User.java
@@ -1,12 +1,7 @@
 package tech.noetzold.anPerformaticEcommerce.security.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.util.Collection;
 import java.util.List;
 import lombok.AllArgsConstructor;
