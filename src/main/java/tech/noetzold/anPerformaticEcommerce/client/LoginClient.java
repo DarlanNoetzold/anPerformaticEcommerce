d7950c8f5598700bb96efea2320feec6eb8b5d5d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-11-29 07:45:39 -0300

    Update LoginClient.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/client/LoginClient.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/client/LoginClient.java
@@ -1,19 +1,19 @@
 package tech.noetzold.anPerformaticEcommerce.client;
 
-
 import feign.Headers;
+import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Service
-@FeignClient(url= "http://localhost:8091" , name = "login")
+@FeignClient(url= "http://localhost:8180" , name = "login")
 public interface LoginClient {
 
     @RequestMapping(value = "/realms/quarkus1/protocol/openid-connect/token", method = RequestMethod.POST,
             consumes = "application/x-www-form-urlencoded")
-    @Headers("Content-Type: application/x-www-form-urlencoded")
-    String getToken(@RequestBody String requestBody);
-}
+    @Headers({"Content-Type: application/x-www-form-urlencoded"})
+    String getToken(@RequestBody String requestBody, @RequestHeader("Authorization") String authorizationHeader);}
