ad0e2fd5fbb41933a234b8b18e05860c5ccac15a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-01 23:27:40 -0300

    Update CatalogClient.java

--- src/main/java/tech/noetzold/anPerformaticEcommerce/client/CatalogClient.java
+++ src/main/java/tech/noetzold/anPerformaticEcommerce/client/CatalogClient.java
@@ -2,8 +2,15 @@ package tech.noetzold.anPerformaticEcommerce.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import tech.noetzold.anPerformaticEcommerce.model.payment.PaymentModel;
 
 @Service
 @FeignClient(url= "http://localhost:5000/api/catalog/v1/attribute" , name = "catalog")
 public interface CatalogClient {
+
+    @GetMapping("/sendUpdateNotification")
+    void getPayment(@RequestHeader(value = "Authorization", required = true) String authorizationHeader);
 }
